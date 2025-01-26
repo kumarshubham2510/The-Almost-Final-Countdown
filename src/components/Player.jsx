f27cd633440f10e6c566f8b1e6058dfb0d23316d
Kumar Shubham <kumarshubhams2510@gmail.com>
2025-01-27 00:24:35 +0530

    player name logic added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleChange} value={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
