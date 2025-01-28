fa183430baff05e6637b045379130f572cbb0759
Kumar Shubham <kumarshubhams2510@gmail.com>
2025-01-28 09:37:11 +0530

    timerChallenger ui added

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,37 @@
+import { useState, useRef } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  let timer = useRef();
+
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, 2000);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You Lost</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : ""}>
+        {timerStarted ? "Time is running ..." : "Time inactive"}
+      </p>
+    </section>
+  );
+}
